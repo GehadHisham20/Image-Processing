2b749de29827f00bbd06372cde18bcbfe555c930
tokamh <73889436+tokamh@users.noreply.github.com>
2022-04-20 11:38:38 -0700

    Add files via upload

---
+++ Runner.bat
@@ -0,0 +1,3 @@
+@echo off
+"C:\Users\A S U S\AppData\Local\Programs\Python\Python37\scripts\ipython.exe" "C:\Users\A S U S\OneDrive\Desktop\AdvancedLaneLinesDetection\LaneLineFinder.py" d:\challenge_video.mp4 d:\53.mp4 debug
+pause
