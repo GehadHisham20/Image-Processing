edc463526aabc01f2c1359704b5c7d535330ac9d
tokamh <73889436+tokamh@users.noreply.github.com>
2022-04-20 04:21:47 -0700

    Update inProgress.py

--- inProgress.py
+++ inProgress.py
@@ -688,4 +688,58 @@ for frame_path in test_img_paths:
         img_overlay = cv2.addWeighted(out_unwarped, 0.5, img, 0.5, 0)
         plot_images([(warped, 'Original'), (out, 'Out'), (img_overlay, 'Overlay')], figsize=(20, 18))
 
+        #-----------------Pixels to Meters-------------------
+def compute_mppx(img, dashed_line_loc, visualise=False):
+    lane_width = 3.7
+    dashed_line_len = 3.048
+    
+    if dashed_line_loc == 'left':
+        y_top = 295
+        y_bottom = 405
+    elif dashed_line_loc == 'right':
+        y_top = 395
+        y_bottom = 495
         
+    binary = BinaryImage(img)
+    histogram = np.sum(binary[int(binary.shape[0] / 2):, :], axis=0)
+    midpoint = np.int(histogram.shape[0] / 2)
+    
+    x_left = np.argmax(histogram[:midpoint])
+    x_right = np.argmax(histogram[midpoint:]) + midpoint
+    
+    x_mppx = lane_width / (x_right - x_left)
+    y_mppx = dashed_line_len / (y_bottom - y_top)
+    
+    if visualise:
+        plt.figure(figsize=(10, 8))
+        plt.imshow(img)
+        plt.axis('off')
+        
+        if dashed_line_loc == 'left':
+            plt.plot((x_left, x_left), (y_top, y_bottom), 'r')
+            plt.text(x_left + 10, (y_top + y_bottom) // 2, '{} m'.format(dashed_line_len), color='r', fontsize=20)
+
+        elif dashed_line_loc == 'right':
+            plt.plot((x_right, x_right), (y_top, y_bottom), 'r')
+            plt.text(x_right + 10, (y_top + y_bottom) // 2, '{} m'.format(dashed_line_len), color='r',fontsize=20)
+
+        plt.plot((x_left, x_right), (img.shape[0] - 200 , img.shape[0] - 200), 'r')
+        plt.text((x_left + x_right) // 2, img.shape[0] - 220, '{} m'.format(lane_width), color='r', fontsize=20)
+        
+    return y_mppx, x_mppx
+#--------------------Determine avg meters--------------
+if 6 in plot_demo:
+    visualise = True
+else:
+    visualise = False
+    
+img, _ = get_image(test_img_paths[0])
+y_mppx1, x_mppx1 = compute_mppx(img, dashed_line_loc='right', visualise=visualise)
+img, _ = get_image(test_img_paths[1])
+y_mppx2, x_mppx2 = compute_mppx(img, dashed_line_loc='left', visualise=visualise)
+
+x_mppx = (x_mppx1 + x_mppx2) / 2
+y_mppx = (y_mppx1 + y_mppx2) / 2
+
+print('The avgerage meter/px (x-axis): {:.4f}'.format(x_mppx))
+print('The avgerage meter/px (y-axis): {:.4f}'.format(y_mppx))
