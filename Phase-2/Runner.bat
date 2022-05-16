60e8f7496c21a23d603d0d3b05f251f16ee55328
tokamh <73889436+tokamh@users.noreply.github.com>
2022-05-16 08:17:39 -0700

    Add files via upload

---
+++ Phase-2/Runner.bat
@@ -0,0 +1,3 @@
+@echo off
+"C:\Users\A S U S\AppData\Local\Programs\Python\Python37\scripts\ipython.exe" "D:\final year\2nd\image project phase 2\try\VehicleDetector.py" D:\project_video.mp4 D:\out.mp4 debug
+pause
